d419a604cc9a935bea67d227ef6a5c726574cb17
Mizgirev Yaroslav <mizgirev.yaroslav@gmail.com>
2023-01-31 10:12:06 +0300

    Update Main.java

--- spring-xml-gradle/src/main/java/com/mym/Main.java
+++ spring-xml-gradle/src/main/java/com/mym/Main.java
@@ -1,7 +1,16 @@
 package com.mym;
 
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
+  
+  private static final Logger LOGGER = LogManager.getLogger(Starter.class);
+  
   public static void main(String[] args) {
-    // enter you code
+    
+    LOGGER.log(Level.INFO, "Hello!");
+    
   }
 }
