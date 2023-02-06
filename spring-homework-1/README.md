38875d5633a6472f8f776314474a96911281762e
Mizgirev Yaroslav <mizgirev.yaroslav@gmail.com>
2023-02-06 20:01:45 +0300

    Delete spring-homework-1 directory

--- spring-homework-1/README.md
+++
@@ -1,1 +0,0 @@
-# Spring: XML config.
--- spring-homework-1/src/main/java/com/mym/homework/dao/Dao.java
+++
@@ -1,8 +0,0 @@
-package com.mym.homework.dao;
-
-import java.io.IOException;
-import java.util.Collection;
-
-public interface Dao<T> {
-    Collection<T> getAllQuestions(String filename, String delimiter) throws IOException;
-}
