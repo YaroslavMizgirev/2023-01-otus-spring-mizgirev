ece1ab5b530adbeca29f4c33345f34d8e76277de
Yaroslav Mizgirev <yaroslav@mizgirev.ru>
2023-02-06 20:35:36 +0300

    Update branch 'homework-1'

--- spring-homework-1/src/main/java/com/mym/homework/Main.java
+++ spring-homework-1/src/main/java/com/mym/homework/Main.java
@@ -1,6 +1,5 @@
 package com.mym.homework;
 
-import com.mym.homework.dao.InterviewParser;
 import com.mym.homework.service.InterviewProcess;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
@@ -10,7 +9,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import java.io.IOException;
 
 public class Main {
-    private final static Logger LOGGER = LogManager.getLogger(InterviewParser.class);
+    private final static Logger LOGGER = LogManager.getLogger(Main.class);
     public static void main(String[] args) throws IOException {
         var context = new ClassPathXmlApplicationContext("/spring-context.xml");
         var interviewProcess = context.getBean(InterviewProcess.class);
